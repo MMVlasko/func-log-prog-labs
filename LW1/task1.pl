419ec8f71a3fdf204011febb1d359b68ea4a254c
MMVlasko <mmvlasko@mai.education>
2024-10-30 16:20:46 +0300

    fix lw1

--- LW1/task1.pl
+++ LW1/task1.pl
@@ -3,17 +3,17 @@ my_length([_ | T], Length) :-
     my_length(T, TLength), 
     Length is TLength + 1.
 
-remove([H | T], H, T).
-remove([H | T1], X, [H | T2]) :- 
-    remove(T1, X, T2).
+my_remove(H, [H | T], T).
+my_remove(X, [H | T1], [H | T2]) :- 
+    my_remove(X, T1, T2).
 
-permute([], []).
-permute(L, [H | Perm]) :- 
-    remove(L, H, L1), 
-    permute(L1, Perm).
+my_permute([], []).
+my_permute(L, [H | Perm]) :- 
+    my_remove(H, L, L1), 
+    my_permute(L1, Perm).
 
 my_append([], X, X).
-my_append([H, T1], Y, [H | T2]) :- 
+my_append([H | T1], Y, [H | T2]) :- 
     my_append(T1, Y, T2).
 
 my_sublist([], _).
@@ -28,22 +28,23 @@ my_member(H, [_ | T]) :-
 
 
 % удаление последних трёх элементов без использования стандартных предикатов
-rm_last_three_elems([_], []).
-rm_last_three_elems([_, _], []).
-rm_last_three_elems([_, _, _], []).
-rm_last_three_elems([H1 | T1], [H1 | T2]) :- 
-    rm_last_three_elems(T1, T2).
+rm_last_three([], []).
+rm_last_three([_], []) :- !.
+rm_last_three([_, _], []) :- !.
+rm_last_three([_, _, _], []) :- !.
+rm_last_three([H1 | T1], [H1 | T2]) :- 
+    rm_last_three(T1, T2).
 
 % удаление последних трёх элементов с использованием стандартных предикатов
-rm_last_three_elems_with_std_preds(X, []) :- 
-    my_length(X, L), 
-    L < 4.
-rm_last_three_elems_with_std_preds([H1 | T1], [H1 | T2]) :- 
-    rm_last_three_elems_with_std_preds(T1, T2).
+rm_last_three_std(X, []) :- 
+    length(X, L), 
+    L < 4, !.
+rm_last_three_std([H1 | T1], [H1 | T2]) :- 
+    rm_last_three_std(T1, T2).
 
 
 % подсчёт количества элементов числового списка и их суммы
-sum_and_count([H], H, 1).
+sum_and_count([H], H, 1) :- !.
 sum_and_count([H | T], Sum, Count) :- 
     sum_and_count(T, S, C), 
     Sum is S + H, 
@@ -55,7 +56,7 @@ middle(L, X) :-
     X is S / C.
 
 % поиск среднего арифметического с стандартными предикатами
-middle_with_std_preds(X, M) :- 
+middle_std(X, M) :- 
     sumlist(X, S), 
     length(X, L), 
     M is S / L.
--- LW1/task2.pl
+++ LW1/task2.pl
@@ -1,40 +1,3 @@
-subject('LP','Логическое программирование').
-subject('MTH','Математический анализ').
-subject('FP','Функциональное программирование').
-subject('INF','Информатика').
-subject('ENG','Английский язык').
-subject('PSY','Психология').
-
-student(102,'Петров',[grade('LP',3),grade('MTH',4),grade('FP',4),grade('INF',4),grade('ENG',5),grade('PSY',4)]).
-student(101,'Петровский',[grade('LP',5),grade('MTH',4),grade('FP',3),grade('INF',4),grade('ENG',2),grade('PSY',4)]).
-student(104,'Иванов',[grade('LP',4),grade('MTH',5),grade('FP',5),grade('INF',4),grade('ENG',3),grade('PSY',3)]).
-student(102,'Ивановский',[grade('LP',3),grade('MTH',4),grade('FP',3),grade('INF',3),grade('ENG',3),grade('PSY',5)]).
-student(104,'Запорожцев',[grade('LP',3),grade('MTH',3),grade('FP',3),grade('INF',5),grade('ENG',5),grade('PSY',2)]).
-student(101,'Сидоров',[grade('LP',5),grade('MTH',3),grade('FP',5),grade('INF',5),grade('ENG',4),grade('PSY',2)]).
-student(103,'Сидоркин',[grade('LP',4),grade('MTH',4),grade('FP',2),grade('INF',3),grade('ENG',4),grade('PSY',3)]).
-student(102,'Биткоинов',[grade('LP',4),grade('MTH',5),grade('FP',5),grade('INF',3),grade('ENG',3),grade('PSY',4)]).
-student(103,'Эфиркина',[grade('LP',4),grade('MTH',5),grade('FP',3),grade('INF',3),grade('ENG',4),grade('PSY',4)]).
-student(103,'Сиплюсплюсов',[grade('LP',3),grade('MTH',5),grade('FP',3),grade('INF',4),grade('ENG',3),grade('PSY',4)]).
-student(103,'Программиро',[grade('LP',3),grade('MTH',5),grade('FP',4),grade('INF',3),grade('ENG',5),grade('PSY',4)]).
-student(104,'Джаво',[grade('LP',5),grade('MTH',4),grade('FP',4),grade('INF',5),grade('ENG',3),grade('PSY',4)]).
-student(103,'Клавиатурникова',[grade('LP',3),grade('MTH',2),grade('FP',3),grade('INF',2),grade('ENG',5),grade('PSY',4)]).
-student(101,'Мышин',[grade('LP',5),grade('MTH',5),grade('FP',2),grade('INF',4),grade('ENG',4),grade('PSY',2)]).
-student(104,'Фулл',[grade('LP',5),grade('MTH',4),grade('FP',5),grade('INF',4),grade('ENG',4),grade('PSY',4)]).
-student(101,'Безумников',[grade('LP',5),grade('MTH',4),grade('FP',4),grade('INF',4),grade('ENG',5),grade('PSY',4)]).
-student(102,'Шарпин',[grade('LP',4),grade('MTH',3),grade('FP',2),grade('INF',3),grade('ENG',3),grade('PSY',4)]).
-student(104,'Круглосчиталкин',[grade('LP',5),grade('MTH',4),grade('FP',4),grade('INF',4),grade('ENG',2),grade('PSY',4)]).
-student(103,'Решетников',[grade('LP',3),grade('MTH',3),grade('FP',5),grade('INF',5),grade('ENG',5),grade('PSY',4)]).
-student(102,'Эксель',[grade('LP',4),grade('MTH',4),grade('FP',4),grade('INF',4),grade('ENG',4),grade('PSY',3)]).
-student(102,'Текстописов',[grade('LP',5),grade('MTH',4),grade('FP',5),grade('INF',2),grade('ENG',3),grade('PSY',4)]).
-student(103,'Текстописова',[grade('LP',3),grade('MTH',4),grade('FP',3),grade('INF',4),grade('ENG',4),grade('PSY',4)]).
-student(101,'Густобуквенникова',[grade('LP',4),grade('MTH',5),grade('FP',4),grade('INF',4),grade('ENG',5),grade('PSY',4)]).
-student(102,'Криптовалютников',[grade('LP',4),grade('MTH',3),grade('FP',4),grade('INF',4),grade('ENG',3),grade('PSY',4)]).
-student(104,'Блокчейнис',[grade('LP',4),grade('MTH',2),grade('FP',5),grade('INF',2),grade('ENG',5),grade('PSY',3)]).
-student(102,'Азурин',[grade('LP',5),grade('MTH',2),grade('FP',5),grade('INF',5),grade('ENG',4),grade('PSY',5)]).
-student(103,'Вебсервисов',[grade('LP',4),grade('MTH',5),grade('FP',4),grade('INF',5),grade('ENG',4),grade('PSY',4)]).
-student(102,'Круглотличников',[grade('LP',3),grade('MTH',4),grade('FP',5),grade('INF',3),grade('ENG',4),grade('PSY',5)]).
-
-
 % проверка принадлежности элемента к списку
 my_member(H, [H | _]).
 my_member(H, [_ | T]) :- 
@@ -63,30 +26,35 @@ middle(L, X) :-
     sum_and_count(L, S, C), 
     X is S / C.
 
-% проверка отсутствия несданных экзаменов у даннного студента данной группы
+% проверка отсутствия несданных экзаменов у данного студента данной группы
 pass(Group, Student, Pass) :- 
     student(Group, Student, Grades), 
     structers_to_grades(Grades, L), 
     (my_member(2, L) -> Pass = false ; Pass = true).
 
-% получение среднего арифмитического оценок и информации о наличии 
+% получение среднего арифметического оценок и информации о наличии 
 % несданных экзаменов данного студента данной группы
 student_info(Group, Student, Middle, Pass) :- 
     pass(Group, Student, Pass),
-    middle(L, Middle). % 1
+    grades(Group, Student, L),
+    middle(L, Middle).
+
+% получение среднего балла и факта о сдаче для каждого студента
+students_info(Res) :- 
+    findall(student_info(Group, Student, Middle, Pass), student_info(Group, Student, Middle, Pass), Res). % 1
 
 
 % получение количества вхождений данного элемента в данный список
-count(Elem, [], 0).
+count(_, [], 0).
 count(Elem, [Elem | T], Res) :-
      count(Elem, T, TRes), 
-     Res is TRes + 1.
-count(Elem, [H | T], Res) :- 
-    count(Elem, T, Res).
+     Res is TRes + 1, !.
+count(Elem, [_ | T], Res) :- 
+    count(Elem, T, Res), !.
 
 % получение оценки Grade по данному предмету Subj из списка элементов вида grade(Subj, Grade)
 get_grade(Subj, [grade(Subj, Grade) | _], Grade).
-get_grade(Subj, [H | T], Grade) :- 
+get_grade(Subj, [_ | T], Grade) :- 
     get_grade(Subj, T, Grade).
 
 % получение списка оценок всех студентов по данному предмету
@@ -97,11 +65,15 @@ grades_by_subject(Subj, RGrades) :-
 not_pass_count(SubjName, Count) :- 
     subject(Subj, SubjName), 
     grades_by_subject(Subj, Grades), 
-    count(2, Grades, Count). % 2
+    count(2, Grades, Count).
+
+% получение количества несдавших для всех предметов
+subjects_passing_info(Res) :-
+    findall(not_pass_count(SubjName, Count), not_pass_count(SubjName, Count), Res). % 2
 
 
 % проверка того, что данный элемент больше или равен любого элемента данного списка
-more_than_everybody_in_list(X, []).
+more_than_everybody_in_list(_, []).
 more_than_everybody_in_list(X, [H | T]) :- 
     X >= H, 
     more_than_everybody_in_list(X, T).
@@ -129,4 +101,8 @@ max_middle_student(Group, Student) :-
 
 % получение списка всех студентов с максимальной средней оценкой у данной группы
 max_middle(Group, Students) :- 
-    findall(Student, max_middle_student(Group, Student), Students). % 3
+    setof(Student, max_middle_student(Group, Student), Students).
+
+% получение списков студентов с максимальным средним баллом для каждой группы
+max_middles(Res) :-
+    findall(max_middle(Group, Students), max_middle(Group, Students), Res). % 3
